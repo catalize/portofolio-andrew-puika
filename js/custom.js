aac25373af51aad40d76840aa719cc6dc5bffca8
Catalize <andrewheartnetxiii@gmail.com>
2024-03-27 23:10:54 +0700

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -11,8 +11,17 @@
 
 (function($) {
   'use strict';
+
+
+
   $(function() {
     // Code here executes When the DOM is loaded...
+    // Load partials pages
+    console.log("Hai");
+    $("div[data-includeHTML]").each(function () {                
+        $(this).load($(this).attr("data-includeHTML"));
+        console.log("loaded");
+    });
   });
   $(window).on('load', function() {
     // Code here executes When the page is loaded
